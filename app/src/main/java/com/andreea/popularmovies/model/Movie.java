5359b56b6ad8a45ade8066b085f572347dd6f211
andreea <andreea.mutu3@gmail.com>
2018-02-19 22:00:05 +0200

    create Movie class with movie details

---
+++ app/src/main/java/com/andreea/popularmovies/model/Movie.java
@@ -0,0 +1,52 @@
+package com.andreea.popularmovies.model;
+
+public class Movie {
+    private String originalTitle;
+    private String posterUrl;
+    private String plotSynopsis;
+    private String userRating;
+    private String releaseDate;
+
+    public Movie() {
+    }
+
+    public String getOriginalTitle() {
+        return originalTitle;
+    }
+
+    public String getPosterUrl() {
+        return posterUrl;
+    }
+
+    public String getPlotSynopsis() {
+        return plotSynopsis;
+    }
+
+    public String getUserRating() {
+        return userRating;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setOriginalTitle(String originalTitle) {
+        this.originalTitle = originalTitle;
+    }
+
+    public void setPosterUrl(String posterUrl) {
+        this.posterUrl = posterUrl;
+    }
+
+    public void setPlotSynopsis(String plotSynopsis) {
+        this.plotSynopsis = plotSynopsis;
+    }
+
+    public void setUserRating(String userRating) {
+        this.userRating = userRating;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+}
