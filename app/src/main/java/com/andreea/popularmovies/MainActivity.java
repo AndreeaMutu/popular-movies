469c91812292aa0cd829f622fd949116a1070056
Mutu, Andreea <andreea.mutu@misys.com>
2018-02-23 12:19:02 +0200

    add api_key variable in gradle.properties

--- app/src/main/java/com/andreea/popularmovies/MainActivity.java
+++ app/src/main/java/com/andreea/popularmovies/MainActivity.java
@@ -2,6 +2,9 @@ package com.andreea.popularmovies;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -15,13 +18,24 @@ import com.andreea.popularmovies.model.Movie;
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity implements
+        LoaderManager.LoaderCallbacks<List<Movie>> {
     private static final String TAG = MainActivity.class.getSimpleName();
     public static final int COLUMNS = 2;
+    private static final int MOVIES_LOADER_ID = 77;
     private List<Movie> movieList = new ArrayList<>();
+    private MoviesAdapter adapter;
+    private static final String API_KEY = BuildConfig.API_KEY;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +50,9 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(mLayoutManager);
         recyclerView.setAdapter(adapter);
         adapter.notifyDataSetChanged();
+
+        // init loader
+        getSupportLoaderManager().initLoader(MOVIES_LOADER_ID, null, this);
     }
 
     private void fillListWithMockMovies() {
@@ -47,6 +64,22 @@ public class MainActivity extends AppCompatActivity {
         movieList.add(m1);
         movieList.add(m1);
         movieList.add(m1);
+    }
+
+    @Override
+    public Loader<List<Movie>> onCreateLoader(int id, Bundle args) {
+        return new MoviesAsyncTaskLoader(this);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<Movie>> loader, List<Movie> data) {
+        movieList = data;
+//        adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<Movie>> loader) {
+
     }
 
     private static class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.PosterViewHolder> {
@@ -101,6 +134,48 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public int getItemCount() {
             return movieList.size();
+        }
+    }
+
+    private static class MoviesAsyncTaskLoader extends AsyncTaskLoader<List<Movie>> {
+        private List<Movie> movies;
+
+        public MoviesAsyncTaskLoader(Context context) {
+            super(context);
+            if (movies != null) {
+                deliverResult(movies);
+            } else {
+                forceLoad();
+            }
+        }
+
+        @Override
+        protected void onStartLoading() {
+            super.onStartLoading();
+        }
+
+        @Override
+        public List<Movie> loadInBackground() {
+            OkHttpClient client = new OkHttpClient();
+            String url = "https://api.themoviedb.org/3/movie/popular?page=1&language=en-US&api_key="+API_KEY;
+            Request request = new Request.Builder()
+                    .url(url)
+                    .get()
+                    .build();
+
+            try {
+                Response response = client.newCall(request).execute();
+                Log.d(TAG, "loadInBackground: "+response.body().string());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        public void deliverResult(List<Movie> data) {
+            movies = data;
+            super.deliverResult(data);
         }
     }
 }
