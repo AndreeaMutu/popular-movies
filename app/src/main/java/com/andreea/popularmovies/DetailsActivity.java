5b4e596eb9c3803568609ad15b5b53b3fecae1fa
Mutu, Andreea <andreea.mutu@misys.com>
2018-02-28 18:01:59 +0200

    start populating details layout

--- app/src/main/java/com/andreea/popularmovies/DetailsActivity.java
+++ app/src/main/java/com/andreea/popularmovies/DetailsActivity.java
@@ -2,12 +2,16 @@ package com.andreea.popularmovies;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Toast;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 import com.andreea.popularmovies.model.Movie;
 
 public class DetailsActivity extends AppCompatActivity {
+    private static final String MOVIE_KEY = "Movie";
+    private Movie movie;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,11 +19,23 @@ public class DetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_details);
         Intent moviesIntent = getIntent();
 
-        if (moviesIntent != null) {
-            if (moviesIntent.hasExtra("Movie")) {
-                Movie movie = moviesIntent.getParcelableExtra("Movie");
-                Toast.makeText(this, movie.toString(), Toast.LENGTH_LONG).show();
-            }
+        if (moviesIntent != null && moviesIntent.hasExtra(MOVIE_KEY)) {
+            movie = moviesIntent.getParcelableExtra(MOVIE_KEY);
+        }
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        CollapsingToolbarLayout ctl = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        TextView titleTv = (TextView) findViewById(R.id.movie_title_tv);
+        TextView plotSynopsisTextView = (TextView) findViewById(R.id.plot_synopsis_tv);
+
+        if (movie != null) {
+            ctl.setTitle(movie.getTitle());
+            titleTv.setText(movie.getTitle());
+            plotSynopsisTextView.setText(movie.getOverview());
         }
     }
 }
